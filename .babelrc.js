7678b49fffcd925f58616569893f33daed171b3b
abhishekgupta01 <abhishekgupta@gofynd.com>
2022-05-20 09:47:54 +0530

    Revert "added rollup"
    This reverts commit 4c14edfbdd944c9d338df3d227454ae3deb93097.
    
    reverting

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,4 @@
 module.exports = {
     presets: ["@babel/preset-react", ['@babel/preset-env', { targets: { node: "current" } }]],
-    // plugins: ["@babel/plugin-transform-runtime", "@babel/plugin-transform-modules-commonjs"]
+    plugins: ["@babel/plugin-transform-runtime", "@babel/plugin-transform-modules-commonjs"]
 };
